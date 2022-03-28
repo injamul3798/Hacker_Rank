5e642a99919a750fe38c094481826cefbb4fc5c9
injamul3798 <101572467+injamul3798@users.noreply.github.com>
2022-03-28 18:13:34 +0600

    Create Angry Professor.cpp

---
+++ Angry Professor.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int T;
+    int n,k,i,j;
+    cin>>T;
+    for(j=1; j<=T; j++)
+    {
+        cin>>n>>k;
+        int arr[n];
+        for(i=0; i<n; i++)
+        {
+            cin>>arr[i];
+        }
+        int count=0;
+        for(i=0; i<n; i++)
+        {
+            if(arr[i]<=0)
+            {
+                count++;
+            }
+        }
+       /* if(count>k)
+        {
+            cout<<"NO"<<endl;
+        }
+        else
+        {
+            cout<<"YES"<<endl;
+        }*/
+        cout<<((count >= k) ? "NO" : "YES")<<endl;
+    }
+    return 0;
+}

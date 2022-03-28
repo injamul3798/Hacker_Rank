051345a260cc02cf0050d1cc9238497d61c5aa72
injamul3798 <101572467+injamul3798@users.noreply.github.com>
2022-03-28 18:30:02 +0600

    Create Correctness and the Loop Invariant.c

---
+++ Correctness and the Loop Invariant.c
@@ -0,0 +1,57 @@
+
+//Md. Injamul Haque
+//BSc in CSE ,DIU
+#include<stdio.h>
+int Partition(int array[],int left,int Right){
+    int temp,pivot;
+    pivot=array[left];
+    while(left!=Right){
+        if(pivot==array[left]){
+            if(array[Right]>pivot){
+                Right--;
+            }
+            else if(array[Right]<=pivot){
+            temp=array[Right];
+            array[Right]=array[left];
+            array[left]=temp;
+            pivot=array[Right];
+            left++;
+           }
+        }
+        else if(pivot==array[Right]){
+            if(array[left]<pivot){
+                left++;
+            }
+            else if(array[left]>=pivot){
+                temp=array[left];
+                array[left]=array[Right];
+                array[Right]=temp;
+                pivot=array[left];
+                Right--;
+            }
+        }
+
+    }
+    return left;
+}
+void Quicksort(int arr[],int left,int Right){
+    int pivot;
+    if(left<Right){
+        pivot=Partition(arr,left,Right);
+        Quicksort(arr,left,pivot-1);
+        Quicksort(arr,pivot+1,Right);
+    }
+}
+int main(){
+    int n,i;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    Quicksort(arr,0,n-1);
+
+     for(i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+}
